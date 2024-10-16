5e04082c9ed5ddc0d9865eddfad4dcd220f92053
Joshua Cortes <155915932+BochiniSGR@users.noreply.github.com>
2024-10-16 17:44:08 -0300

    Clase de preguntas T o F completa

--- Programacion Proyecto Examen/src/TFpregunta.java
+++ Programacion Proyecto Examen/src/TFpregunta.java
@@ -2,11 +2,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+import java.util.Scanner;
 /**
  *
  * @author Joshu
  */
 public class TFpregunta extends Pregunta{
-    
+    private boolean respCorrecta;
+
+    public TFpregunta(String text , boolean respCorrecta, int puntaje ){
+        super(text, puntaje);
+        this.respCorrecta = respCorrecta;
+    }
+
+@Override
+public boolean buscar(){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("\n>>>>>>"+getText()+"<<<<<<"");
+    System.out.print("Ingrese una opcion verdadero o falso ( T | F )");
+    String respuesta = scan.nextLine().toLowerCase();
+
+    while(!respuesta.equals("t") && !respuesta.equals("f")){
+        System.out.print("Su respuesta no es valida, intentelo otra vez ( T | F )");
+        respuesta = scan.nextLine().toLowerCase();
+    }
+
+    boolean respUsuario = respuesta.equals("t");
+    if(respUsuario == respCorrecta){
+        return true;
+    }else{
+        return false;
+    }
+}      
 }
