97ce292d6f79cc6106ebdd4e7374735013dd2e43
xshure <165109043+xshure@users.noreply.github.com>
2024-10-16 21:29:43 -0300

    Update Resp_Cortas_Pregunta.java
    Resp_Cortas

--- Programacion Proyecto Examen/src/Resp_Cortas_Pregunta.java
+++ Programacion Proyecto Examen/src/Resp_Cortas_Pregunta.java
@@ -5,8 +5,40 @@
 
 /**
  *
- * @author Joshu
+ * @author Hector
  */
-public class Resp_Cortas_Pregunta extends Pregunta {
-    
-}
+import java.util.Scanner;
+
+public class Resp_Cortas extends pregunta{
+    private final String respuestacorrecta;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public Resp_Cortas(String text, String respuestaCorrecta, int puntaje) {
+    super (puntaje, text);
+    this.respuestacorrecta=respuestaCorrecta;
+    }
+    @Override
+    public boolean buscar () {
+    System.out.println (getText ());
+    String respuestaUsuario = leerRespuesta ();
+
+    if (respuestacorrecta.equalsIgnoreCase (respuestaUsuario)) {
+    //System. out.println("¡bien hecho lacayo!");
+    return true;
+    }else{
+    //System.out.println ("equivocado. La respuesta correcta era: " + respuestacorrecta);
+    return false;
+
+    }
+
+    }
+
+    String leerRespuesta () {
+
+    System.out.println("ingresa tu respuesta: ");
+    String respuesta = scanner.nextLine();
+
+
+    return respuesta;
+    }
+    }
